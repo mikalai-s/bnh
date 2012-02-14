4b8f19be876c1477782b441ba44672416eb33c37
Mikalais <mikalai@msilivonik.com>
2012-02-13 23:05:34 -0700

    1. added more styles
    2. added extension methods
    3. added BnhWebFramework project

---
+++ BnhWebFramework/HtmlExtensions.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Routing;
+
+namespace BnhWebFramework
+{
+    // Summary:
+    //     Represents support for HTML links in an application.
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString ActionMenuItem(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName)
+        {
+            var tag = new TagBuilder("li");
+
+            if (htmlHelper.ViewContext.RequestContext.IsCurrentRoute(null, controllerName, actionName))
+            {
+                tag.AddCssClass("current");
+            }
+
+            tag.InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToString();
+
+            return MvcHtmlString.Create(tag.ToString());
+        }
+    }
+}
