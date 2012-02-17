49671cb2bf9039c0dc1a90421ba5b351821f359e
Mikalais <mikalai@msilivonik.com>
2012-02-16 22:09:20 -0700

    more UI changes to community and buider actions

--- BnhWebFramework/HtmlExtensions.cs
+++ BnhWebFramework/HtmlExtensions.cs
@@ -1,5 +1,7 @@
 ﻿
+using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 using System.Web.Routing;
@@ -22,6 +24,36 @@ namespace BnhWebFramework
             tag.InnerHtml = htmlHelper.ActionLink(linkText, actionName, controllerName).ToString();
 
             return MvcHtmlString.Create(tag.ToString());
+        }
+
+        public static MvcHtmlString ActionInputLink(this HtmlHelper htmlHelper, string linkText, string actionName, string controllerName, RouteValueDictionary routeValues, IDictionary<string, object> htmlAttributes)
+        {
+            var inputBuilder = new TagBuilder("input");
+            inputBuilder.Attributes["value"] = linkText;
+            inputBuilder.Attributes["type"] = "button";
+            
+
+            string href = UrlHelper.GenerateUrl(null, actionName, controllerName, null, null, null, routeValues, htmlHelper.RouteCollection, htmlHelper.ViewContext.RequestContext, true);
+            inputBuilder.Attributes["onclick"] = "location.href='" + href + "'";
+            //var linkBuilder = GetLinkBuilder(linkText, htmlAttributes, href);
+
+            //linkBuilder.InnerHtml = inputBuilder.ToString(TagRenderMode.Normal);
+
+            return MvcHtmlString.Create(inputBuilder.ToString(TagRenderMode.Normal)); ;
+        }
+
+        private static TagBuilder GetLinkBuilder(string linkText, IDictionary<string, object> htmlAttributes, string href)
+        {
+            TagBuilder builder = new TagBuilder("a");
+            builder.InnerHtml = !string.IsNullOrEmpty(linkText) ? HttpUtility.HtmlEncode(linkText) : string.Empty;
+            builder.MergeAttributes<string, object>(htmlAttributes);
+            builder.MergeAttribute("href", href);
+            return builder;
+        }
+
+        public static MvcHtmlString ActionInputLink(this HtmlHelper htmlHelper, string linkText, string actionName)
+        {
+            return ActionInputLink(htmlHelper, linkText, actionName, null, new RouteValueDictionary(), new RouteValueDictionary());
         }
     }
 }
