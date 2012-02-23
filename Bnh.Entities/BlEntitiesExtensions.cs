5655f9d542f9806ec05caf7864da41afda3bb144
Mikalai <mikalai@msilivonik.com>
2012-02-23 16:17:10 -0700

    1. added wallscene partial view
    2. made it's possible to go to wall editing from community page

--- Bnh.Entities/BlEntitiesExtensions.cs
+++ Bnh.Entities/BlEntitiesExtensions.cs
@@ -7,6 +7,16 @@ namespace Bnh.Entities
 {
     public static class BlEntitiesExtensions
     {
+        public static Wall GetWall(this Community entity)
+        {
+            return GetEntityWall(entity.Id);
+        }
+
+        public static Wall GetWall(this Builder entity)
+        {
+            return GetEntityWall(entity.Id);
+        }
+
         public static IEnumerable<Brick> GetWallBricks(this Community entity)
         {
             return GetBricksFromEntityWall(entity.Id);
@@ -15,6 +25,23 @@ namespace Bnh.Entities
         public static IEnumerable<Brick> GetWallBricks(this Builder entity)
         {
             return GetBricksFromEntityWall(entity.Id);
+        }
+
+        private static Wall GetEntityWall(Guid entityId)
+        {
+            using (var db = new CmEntities())
+            {
+                var wall = db.Walls.FirstOrDefault(w => w.OwnerId == entityId);
+                if(wall == null)
+                {
+                    wall = db.Walls.CreateObject();
+                    wall.OwnerId = entityId;
+                    wall.Title = string.Empty;
+                    db.Walls.AddObject(wall);
+                    db.SaveChanges();
+                }
+                return wall;
+            }
         }
 
         private static IEnumerable<Brick> GetBricksFromEntityWall(Guid entityId)
