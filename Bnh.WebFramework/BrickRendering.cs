bb14af47357cab4efc8d10ff3c62a061629ff9ad
Mikalai <mikalai@msilivonik.com>
2012-02-22 23:10:16 -0700

    1. added brick renderer
    2. split walldesigner.css into wall.css and walldesigner.css
    3. rendering community wall in usual mode

--- Bnh.WebFramework/BrickRendering.cs
+++ Bnh.WebFramework/BrickRendering.cs
@@ -2,10 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Web.Mvc;
+using Bnh.Entities;
 
 namespace Bnh.WebFramework
 {
     public static class BrickRendering
     {
+        public static MvcHtmlString Brick(this HtmlHelper htmlHelper, Brick brick)
+        {
+            var brickDiv = new TagBuilder("div");
+            brickDiv.AddCssClass("brick");
+
+            var htmlBrick = brick as HtmlBrick;
+            if (htmlBrick != null)
+            {
+                brickDiv.InnerHtml = htmlBrick.Html;
+            }
+
+            
+
+            var wrapperDiv = new TagBuilder("div");
+            wrapperDiv.AddCssClass("brick-wrapper");
+            wrapperDiv.Attributes["style"] = "width: " + brick.Width.ToString("F") + "%";
+            wrapperDiv.InnerHtml = brickDiv.ToString();
+
+            return MvcHtmlString.Create(wrapperDiv.ToString());
+        }
     }
 }
