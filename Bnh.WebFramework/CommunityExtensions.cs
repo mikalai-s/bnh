4f577290f919a1131acae3d7b53607250105c3ea
Mikalai Silivonik <mikalai@msilivonik.com>
2012-04-22 23:24:48 -0600

    updated community edit page to display community filterable properties automatically

--- Bnh.WebFramework/CommunityExtensions.cs
+++ Bnh.WebFramework/CommunityExtensions.cs
@@ -6,6 +6,8 @@ using Bnh.Entities;
 using System.Reflection;
 using System.Web.Script.Serialization;
 using System.Web;
+using System.Web.Mvc;
+using System.Linq.Expressions;
 
 namespace Bnh.WebFramework
 {
@@ -15,24 +17,41 @@ namespace Bnh.WebFramework
         {
             var serializer = new JavaScriptSerializer();
             var clientProperties = new List<string>();
-            var properties = typeof(Community).GetProperties(BindingFlags.Public | BindingFlags.Instance);
             var builder = new StringBuilder("{");
-            foreach(var property in properties)
+            foreach (var jsp in JsProperty.Get(typeof(Community)))
             {
-                if (!Attribute.IsDefined(property, typeof(FilterablePropertyAttribute)))
-                {
-                    continue;
-                }
+                var value = jsp.Property.GetValue(community, null);
 
-                var name = property.Name[0].ToString().ToLower() + property.Name.Substring(1);
-                var value = property.GetValue(community, null);
-
-                builder.AppendFormat("{0}:{1},", name, serializer.Serialize(value));
+                builder.AppendFormat("{0}:{1},", jsp.Name, serializer.Serialize(value));
             }
 
             builder.Append("}");
 
             return builder.ToString();
         }
+
+        public class JsProperty
+        {
+            public IHtmlString Name { get; private set; }
+            public IHtmlString Title { get; private set; }
+            public IHtmlString Operator { get; private set; }
+            
+            public PropertyInfo Property { get; private set; }            
+
+            public static IEnumerable<JsProperty> Get(Type type)
+            {
+                return from property in type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                       where Attribute.IsDefined(property, typeof(FilterablePropertyAttribute))
+                       let name = property.Name[0].ToString().ToLower() + property.Name.Substring(1)
+                       let attr = (property.GetCustomAttributes(typeof(FilterablePropertyAttribute), false).First() as FilterablePropertyAttribute)
+                       select new JsProperty
+                       {
+                           Name = new MvcHtmlString(name),
+                           Title = new MvcHtmlString(attr.Title),
+                           Operator = attr.GetJsOperator(),
+                           Property = property
+                       };
+            }
+        }        
     }
 }
