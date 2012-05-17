9e9ef6f1245afbc45e854c30acf8b3d6509e80a3
Mikalai Silivonik <mikalai@msilivonik.com>
2012-05-16 22:17:23 -0600

    added shared brick support (not finished yet)

---
+++ Bnh.WebFramework/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bnh.WebFramework
+{
+    public static class Constants
+    {
+        public static Guid SharedBricksSceneGuid = new Guid("B740A5E9-3463-4FAC-82C3-DBF9F7BA1017");
+    }
+}
