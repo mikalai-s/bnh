0d1544caa1f51c9836f924f279d0bf077d5a016e
Mikalai Silivonik <mikalai@msilivonik.com>
2012-06-10 23:00:06 -0600

    Fixed scene design

--- Ms.Cms/Models/Brick.cs
+++ Ms.Cms/Models/Brick.cs
@@ -3,22 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
 
 namespace Ms.Cms.Models
 {
     [Table("Bricks", Schema = "Cms")]
     public abstract partial class Brick
     {
+        [DataMemberAttribute()]
         public long Id { get; set; }
         
         [MaxLength(50)]
+        [DataMemberAttribute()]
         public string Title { get; set; }
 
+        [DataMemberAttribute()]
         public float Width { get; set; }
 
+        [DataMemberAttribute()]
         public byte Order { get; set; }
 
         [MaxLength(50)]
+        [DataMemberAttribute()]
         public string ContentTitle { get; set; }
 
         public virtual Wall Wall { get; set; }
--- Ms.Cms/Models/HtmlBrick.cs
+++ Ms.Cms/Models/HtmlBrick.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Ms.Cms.Models
 {
     public partial class HtmlBrick : Brick
     {
+        [DataMemberAttribute()]
         public string Html { get; set; }
     }
     
