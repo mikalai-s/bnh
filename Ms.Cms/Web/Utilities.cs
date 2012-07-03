aa1795b4cc6fe3eab46fdffe60ec3e09c4e217c2
Mikalai Silivonik <mikalai@msilivonik.com>
2012-07-02 22:30:35 -0600

    reorganized views

---
+++ Ms.Cms/Web/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ms.Cms
+{
+    internal class Utilities
+    {
+        //public static MvcHtmlString ResolveUrl(string url)
+        //{
+        //    var ub = new UriBuilder("~");
+        //    ub.Uri.
+        //    MsCms.WebExtractorUrl 
+        //}
+    }
+}
