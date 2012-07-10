6fbf2d5fae4a5883f4535d25b17ca9cfae89d831
Mikalai Silivonik <mikalai@msilivonik.com>
2012-07-09 22:40:55 -0600

    fixed data initialization for MongoDB
    removed SQL scripts

---
+++ Bnh.Web/Controllers/InitData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using Bnh.Web.Models;
+using MongoDB.Driver;
+using System.Configuration;
+
+namespace Bnh.Web.Controllers
+{
+    public static class InitData
+    {
+        public static void Init()
+        {
+            // simple check whether there is need to initialize database data
+            if (Membership.GetUserNameByEmail("admin@bnh.com") == "admin")
+                return;
+
+            var user = Membership.CreateUser("user@bnh.com", "1");
+            user.Email = user.UserName;
+
+            var userProfile = AccountProfile.GetProfile(user.UserName);
+            userProfile.Gender = AccountProfile.GenderEnum.Male;
+            userProfile.FirstName = "Bob";
+            userProfile.LastName = "Man";
+            userProfile.Birthday = new DateTime(1969, 1, 6);
+            userProfile.Save();
+
+
+            var admin = Membership.CreateUser("admin@bnh.com", "1");
+            admin.Email = admin.UserName;
+
+            var adminProfile = AccountProfile.GetProfile(admin.UserName);
+            adminProfile.Gender = AccountProfile.GenderEnum.Female;
+            adminProfile.FirstName = "Alice";
+            adminProfile.LastName = "Baker";
+            adminProfile.Birthday = new DateTime(1979, 5, 16);
+            adminProfile.Save();
+
+
+            Roles.CreateRole("content_manager");
+            Roles.AddUserToRole(admin.UserName, "content_manager");
+
+
+        }
+    }
+}
