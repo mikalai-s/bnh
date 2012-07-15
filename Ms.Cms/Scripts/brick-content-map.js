d6756aaf7733c1ec0d6be13609f0d28b8e00aca0
Mikalai Silivonik <mikalai@msilivonik.com>
2012-07-15 01:34:40 -0600

    fixed reset of the scripts

---
+++ Ms.Cms/Scripts/brick-content-map.js
@@ -0,0 +1,22 @@
+﻿define(
+    ["jquery", "map"],
+    function ($, Map) {
+        "use strict";
+
+        var elem = $(".map-canvas-wrapper");
+        var gpsLocationAttr = elem.attr("gpsLocation");
+        var gpsBoundsAttr = elem.attr("gpsBounds");
+
+        var gpsLocation = gpsLocationAttr ? $.parseJSON(gpsLocationAttr) : null;
+        var gpsBounds = gpsBoundsAttr ? $.parseJSON(gpsBoundsAttr) : null;
+
+        var map = new Map($("#mapCanvas").get(0), {
+            zoom: elem.attr("zoom") * 1,
+            center: gpsLocation
+        });
+
+        if (gpsBounds) {
+            map.addPolygon($.map(gpsBounds, map.deserializeCoordinates));
+        }
+    }
+);
