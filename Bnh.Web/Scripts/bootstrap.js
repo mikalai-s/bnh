932ac6d16fd9f0a9746e02f9c36c2ac79f5b0282
Mikalai Silivonik <mikalai@msilivonik.com>
2012-07-15 00:21:04 -0600

    reorganized scripts, wrapped with define() function

---
+++ Bnh.Web/Scripts/bootstrap.js
@@ -0,0 +1,8 @@
+﻿require.config({
+    paths: {
+        jquery: 'libs/jquery/jquery-1.7.2.min' // '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min'
+        , knockout: 'libs/knockout/knockout-2.1.0'
+        , async: 'libs/require/async'
+        , jqballoon: 'libs/jquery/jquery.balloon'
+    }
+});
---
+++ Bnh.Web/Scripts/community-edit.js
@@ -0,0 +1,27 @@
+﻿define(
+    ["jquery", "map-editor"], 
+    function ($, MapEditor) {
+        "use strict"
+
+        var gpsLocationField = $("#GpsLocation");
+        var gpsBoundsField = $("#GpsBounds");
+
+        var overlays = {
+            marker: gpsLocationField.val() ? $.parseJSON(gpsLocationField.val()) : null,
+            polygon: gpsBoundsField.val() ? $.parseJSON(gpsBoundsField.val()) : null,
+            zoom: 14
+        };
+
+        var mapEditor = new MapEditor("#mapCanvas", overlays);
+
+        $("form").validate({
+            submitHandler: function(form) {
+                gpsLocationField.val($.toJSON(mapEditor.getLocation()));
+                gpsBoundsField.val($.toJSON(mapEditor.getBounds()));
+
+                // do other stuff for a valid form
+                form.submit();
+            }
+        });
+    }
+);
