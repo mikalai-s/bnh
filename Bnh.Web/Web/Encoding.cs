5fd7f60ac5e0e9e4f643c96fc7157984964a5f9a
Mikalai Silivonik <mikalai@msilivonik.com>
2012-08-19 19:50:19 -0600

    moved files from webframework project into bnh.web

---
+++ Bnh.Web/Web/Encoding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bnh.WebFramework
+{
+    public static class Encoding
+    {
+        public static string ToBase64(this string input)
+        {
+            return Convert.ToBase64String(ASCIIEncoding.Default.GetBytes(input));
+        }
+
+        public static string FromBase64(this string input)
+        {
+            return ASCIIEncoding.Default.GetString(Convert.FromBase64String(input));
+        }
+    }
+}
---
+++ Bnh.Web/Web/UrlExtensions.cs
@@ -0,0 +1,50 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="UrlExtensions.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Bnh.WebFramework
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public static class UrlExtensions
+    {
+        public static bool IsCurrentRoute(this RequestContext context, string areaName, string controllerName, params string[] actionNames)
+        {
+            var routeData = context.RouteData;
+            var routeArea = routeData.DataTokens["area"] as string;
+
+            if (((string.IsNullOrEmpty(routeArea) && string.IsNullOrEmpty(areaName)) ||
+                  (routeArea == areaName)) &&
+                 ((string.IsNullOrEmpty(controllerName)) ||
+                  (routeData.GetRequiredString("controller") == controllerName)) &&
+                 ((actionNames == null) ||
+                   actionNames.ToArray().Contains(routeData.GetRequiredString("action"))))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool IsCurrent(this UrlHelper urlHelper, string areaName, string controllerName, params string[] actionNames)
+        {
+            return urlHelper.RequestContext.IsCurrentRoute(areaName, controllerName, actionNames);
+        }
+
+        public static string Selected(this UrlHelper urlHelper, string areaName, string controllerName, params string[] actionNames)
+        {
+            return urlHelper.IsCurrent(areaName, controllerName, actionNames) ? "selected" : string.Empty;
+        }
+    }
+}
