05f5e20e30a4f4b6a0578a176d9f31bcd9815758
Mikalai Silivonik <mikalai@msilivonik.com>
2012-12-30 23:13:14 -0700

    added ELMAH

---
+++ Bnh.Web/Helpers/MongoErrorLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bnh.Core;
+using SpellChecker.Net.Search.Spell;
+
+namespace Bnh.Web.Helpers
+{
+    /// <summary>
+    /// A simple derivee just to resolve corrrect connection strings
+    /// </summary>
+    public class MongoErrorLog : Elmah.MongoErrorLog
+    {
+        public MongoErrorLog(System.Collections.IDictionary config)
+            : base(config)
+        {
+        }
+
+        public override string GetConnectionString(System.Collections.IDictionary configSettings)
+        {
+            var config = DependencyResolver.Current.GetService<Config>();
+            if (config == null)
+            {
+                throw new Exception("Config is not registered!");
+            }
+
+            return config.ConnectionStrings[(string)configSettings["connectionStringName"]];
+        }
+        
+    }
+}
