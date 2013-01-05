e4f3f7ebf95f667996e9283ba9e071066c4d1f75
Mikalai Silivonik <mikalai@msilivonik.com>
2013-01-04 23:53:01 -0700

    added admin controller and feedback report view

---
+++ Bnh.Web/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bnh.Core;
+
+namespace Bnh.Web.Controllers
+{
+    [Authorize(Roles = "content_manager")]
+    public class AdminController : Controller
+    {
+        IEntityRepositories Entities { get; set; }
+
+        public AdminController(IEntityRepositories entities)
+        {
+            this.Entities = entities;
+        }
+
+        //
+        // GET: /Admin/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FeedbackReport()
+        {
+            return View(this.Entities.Feedback.OrderBy(c => c.Created));
+        }
+    }
+}
