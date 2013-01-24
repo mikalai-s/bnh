3b93b5672cbbb7ceec1f6f6f7452d36454a714cb
Mikalai Silivonik <msilivonik@gmail.com>
2013-01-23 23:42:47 -0700

    gallery brick editing works (only remote images)

---
+++ Bnh.Web/Areas/Cms/Scripts/edit-gallery-brick.js
@@ -0,0 +1,32 @@
+﻿/*global define, tinymce*/
+
+define(
+    ["jquery"],
+    function () {
+        "use strict";
+
+        var remove = function () {
+
+            var $this = $(this);
+
+            $this.parent().remove();
+
+        };
+
+
+        $(".button-remove").click(remove);
+
+        $("#button-add").click(function () {
+
+            var $newImagesDiv = $("#new-images");
+
+            $newImagesDiv
+                .append('<div><input name="images" style="width: 700px;" type="text" /><input class="button-remove btn" type="button" value="Remove" /></div>')
+                .find(".button-remove")
+                .click(remove);
+
+        });
+
+
+    }
+);
