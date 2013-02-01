4d0a02849c9cf2d9a067cc3dec140dc2112c75f8
Mikalai Silivonik <msilivonik@gmail.com>
2013-01-31 22:58:57 -0700

    don't show non-first tab content on page load

--- Bnh.Web/Areas/Cms/Scripts/view-tabs-content.js
+++ Bnh.Web/Areas/Cms/Scripts/view-tabs-content.js
@@ -3,15 +3,19 @@
     function ($) {
         "use strict";
 
-        $('a[data-toggle="tab"]').on('shown', function (e) {
-            var tabId = $(this).closest(".brick-wrapper").attr("id");
-            var tabIndex = $(this).closest("li").index();
-            var $associatedBricks = $(this).closest(".wall-wrapper").find(".brick-wrapper.tabid_" + tabId);
+        var $tabs = $('a[data-toggle="tab"]');
+
+        function switchTab($a) {
+            var tabId = $a.closest(".brick-wrapper").attr("id");
+            var tabIndex = $a.closest("li").index();
+            var $associatedBricks = $a.closest(".wall-wrapper").find(".brick-wrapper.tabid_" + tabId);
 
             $associatedBricks.hide();
             $associatedBricks.parent().find(".tabindex_" + tabIndex).show();
+        }
+
+        $tabs.on('shown', function (e) {
+            switchTab($(this));
         });
-
-
     }
 );
