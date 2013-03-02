62a5add45da610593f6f263d07c3f879e5421471
Mikalai Silivonik <msilivonik@gmail.com>
2013-03-01 23:07:49 -0700

    missing file from prev commit

--- Bnh.Web/Areas/Cms/Controllers/LinkablesSceneController.cs
+++ Bnh.Web/Areas/Cms/Controllers/LinkablesSceneController.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using Cms.Core;
+using Cms.Helpers;
+using Cms.Infrastructure;
+using Cms.Models;
 
-namespace Bnh.Web.Areas.Cms.Models
+namespace Cms.Controllers
 {
-    public class LinkablesSceneController
+    [DesignerAuthorize]
+    public class LinkablesSceneController : SceneController
     {
+        IRepositories repos;
+
+        public LinkablesSceneController(IConfig config, IRepositories repos, IRatingCalculator rating)
+            : base (config, repos, rating)
+        {
+            this.repos = repos;
+        }
+
+        protected override ISceneHolder GetSceneHolder(string entityId)
+        {
+            return this.repos.SpecialScenes.Single(h => h.SceneId == Constants.LinkableBricksSceneId);
+        }
+
+        protected override void SaveScene(string entityId, Scene scene)
+        {
+            var holder = this.repos.SpecialScenes.Single(h => h.SceneId == Constants.LinkableBricksSceneId);
+            holder.Scene = scene;
+            this.repos.SpecialScenes.Save(holder);
+        }
+
+
+        public ActionResult EditScene()
+        {
+            return View(ContentUrl.Views.Scene.EditLinkable, (string)null, GetSceneHolder(null));
+        }
     }
 }
