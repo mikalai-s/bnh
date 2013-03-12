b6a86a9567721062e5b47a93d85ad8cf1167d28b
Mikalai Silivonik <msilivonik@gmail.com>
2013-03-11 22:01:32 -0600

    removed obsolete code

--- Bnh.Web/Areas/Cms/AutofacModule.cs
+++ Bnh.Web/Areas/Cms/AutofacModule.cs
@@ -20,39 +20,7 @@ namespace Cms
             base.Load(builder);
 
             builder.RegisterType<Repositories>().InstancePerLifetimeScope();
-            builder.RegisterFilterProvider();
-
-            //builder.Register<IConfig>(c => GetConfiguration()).AsImplementedInterfaces().InstancePerLifetimeScope();
+            builder.RegisterFilterProvider();           
         }
-        /*
-        private IConfig GetConfiguration()
-        {
-            // config file
-            var configFile = HttpContext.Current.Server.MapPath("~/config.json");
-#if !DEBUG
-            // release specific config file
-            var privateConfigFile = HttpContext.Current.Server.MapPath("~/config.release.json");
-#else
-            // debug specific config file
-            var privateConfigFile = HttpContext.Current.Server.MapPath("~/config.debug.json");
-#endif
-            if (!File.Exists(privateConfigFile))
-                throw new Exception("Private config file doesn't exist!");
-
-            try
-            {
-                // deserialize config file into object
-                var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configFile));
-
-                // override its values from private config file
-                JsonConvert.PopulateObject(File.ReadAllText(privateConfigFile), config);
-
-                return config;
-            }
-            catch (Newtonsoft.Json.JsonReaderException e)
-            {
-                throw new ConfigurationErrorsException("Failed reading configuration file.", e);
-            }
-        }*/
     }
 }
