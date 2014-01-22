499ea66dc149528f16eef7d4596d9f8a5519ee66
Mikalai Silivonik <mikalais@pandell.com>
2014-01-22 00:00:59 -0700

    added backup project

---
+++ BackupTool/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BackupTool
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                WriteError("Expected path to a config file!");
+                return;
+            }
+
+            if (!File.Exists(args[0]))
+            {
+                WriteError("Unable to find config file!");
+                return;
+            }
+
+            var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(args[0]));
+
+            // backup:
+            var psi = new ProcessStartInfo(config.MongoDumpCmd.Replace("{DestinationPath}", config.DestinationPath));
+          
+        }
+
+        private static void WriteLine(string str, ConsoleColor color = ConsoleColor.Gray)
+        {
+            var originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(str);
+            Console.ForegroundColor = originalColor;
+        }
+
+        private static void WriteError(string str)
+        {
+            WriteLine(str, ConsoleColor.Red);
+        }
+    }
+}
